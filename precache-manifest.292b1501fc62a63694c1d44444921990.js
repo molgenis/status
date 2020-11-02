f17de73b1737c4a857eb44cc2804a70d1255e7db
pneerincx <pieter.neerincx@gmail.com>
2020-11-02 14:16:17 +0100

    Updates

---
+++ precache-manifest.292b1501fc62a63694c1d44444921990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2c1f99f9f2259bb9db769929cd1c29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ead77c496e6a7539fd0",
+    "url": "./static/css/main.b427710c.chunk.css"
+  },
+  {
+    "revision": "3c7de2db782d29190612",
+    "url": "./static/js/2.31582e92.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "./static/js/2.31582e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ead77c496e6a7539fd0",
+    "url": "./static/js/main.67e0e35b.chunk.js"
+  },
+  {
+    "revision": "dd7b323c43b450bc42d7",
+    "url": "./static/js/runtime-main.237a64a8.js"
+  }
+]);
